01ef99b00ddb5f8ade4be522a538027e9aee92b9
Jessica <jrnewport07@gmail.com>
2020-01-06 18:50:47 -0600

    2.5 Studio pre bonus

--- src/exercises/ArrayListPractice.java
+++ src/exercises/ArrayListPractice.java
@@ -5,5 +5,34 @@ import java.util.Scanner;
 
 public class ArrayListPractice {
     public static void main(String[] args) {
+        ArrayList<Integer> ints = new ArrayList<>();
+
+        ints.add(1);
+        ints.add(1);
+        ints.add(2);
+        ints.add(3);
+        ints.add(5);
+        ints.add(8);
+        ints.add(13);
+        ints.add(21);
+        ints.add(34);
+        ints.add(55);
+
+        ArrayList<Integer> evens = new ArrayList<>();
+        int sum = 0;
+
+        for (int i=0; i<ints.size();i++) {
+            if (ints.get(i) % 2 == 0) {
+                evens.add(ints.get(i));
+                sum += ints.get(i);
+            }
+        };
+
+//        System.out.println(evens);
+        System.out.println(sum);
+
+        ArrayList<String> words = new ArrayList<>();
+        String drSeuss = "I would not, could not, in a box. I would not, could not with a fox.\n" +
+        "I will not eat them in a house. I will not eat them with a mouse.";
     }
 }
---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        Double radius = input.nextDouble();
+        input.close();
+
+//        double pi = 3.14;
+//        double areaOfCircle = 3.14*radius*radius;
+       Double areaOfCircle = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius "+radius+" is: "+areaOfCircle);
+    }
+}
