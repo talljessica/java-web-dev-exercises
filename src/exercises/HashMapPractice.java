39c80eb84bae20162ec8503b0f43d079dc20bf7b
Jessica <jrnewport07@gmail.com>
2020-01-08 17:59:19 -0600

    HashMap practice, value not working properly

--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -6,5 +6,43 @@ import java.util.Scanner;
 
 public class HashMapPractice {
     public static void main(String[] args) {
+        HashMap<Integer, String> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        int newID;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        // Get student names and grades
+        do {
+
+            System.out.print("Student ID: ");
+            newID = input.nextInt();
+//
+
+
+            if (newID != 0) {
+                System.out.print("Student Name: ");
+                String newName;
+                newName = input.nextLine();
+                students.put(newID, newName);
+
+                // Read in the newline before looping back
+                input.nextLine();
+            }
+
+        } while(0 != newID);
+
+        // Print class roster
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
+            System.out.println(student.getKey() + student.getValue());
+            System.out.println(student.getValue());
+
+
+        }
+
+
     }
 }
+
