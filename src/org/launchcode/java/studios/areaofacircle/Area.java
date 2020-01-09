6dadcaa268c27306afcb9493f2e3e60099d8ee43
Jessica <jrnewport07@gmail.com>
2020-01-08 18:00:21 -0600

    playing with bonus but not working

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -7,11 +7,23 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         Double radius = input.nextDouble();
+
+//        do {
+//            if (radius <= 0) {
+//                System.out.println("Please enter valid radius!");
+//                break;
+//            }
+//        }while (true);
         input.close();
+
+//        if (input != double){
+//            System.out.println("Invalid radius");
+//        }else{
 
 //        double pi = 3.14;
 //        double areaOfCircle = 3.14*radius*radius;
-       Double areaOfCircle = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius "+radius+" is: "+areaOfCircle);
+            Double areaOfCircle = Circle.getArea(radius);
+            System.out.println("The area of a circle of radius " + radius + " is: " + areaOfCircle);
+        }
     }
-}
+//}
