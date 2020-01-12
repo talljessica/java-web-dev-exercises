7c6e662ac1d0d9eaa05868e7597b8d4455572030
Jessica <jrnewport07@gmail.com>
2020-01-12 10:27:13 -0600

    added getters and setters to Student.java

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,42 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId){
+        this(name, studentId, 0, 0.0);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+    public double getGpa(){
+        return gpa;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+    public void setNumberOfCredits(int aNumberOfCredits) {
+        numberOfCredits = aNumberOfCredits;
+    }
+    public void setGpa(double aGpa){
+        gpa = aGpa;
+    }
 }
