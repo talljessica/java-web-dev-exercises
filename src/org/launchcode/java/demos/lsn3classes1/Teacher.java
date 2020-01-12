0a4b2541a8dc12bebec20a4fc60cd1e447aacae8
Jessica <jrnewport07@gmail.com>
2020-01-12 11:15:22 -0600

    created Teacher.java

---
+++ src/org/launchcode/java/demos/lsn3classes1/Teacher.java
@@ -0,0 +1,32 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+public class Teacher {
+
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private int yearsTeaching;
+
+    public Teacher (String firstName, String lastName, String subject, int yearsTeaching){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.subject = subject;
+        this.yearsTeaching = yearsTeaching;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setYearsTeaching(int yearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+}
